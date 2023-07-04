d04f4714bd44e2515c2508554e9d16e1933c090f
William Robinet <william.robinet@conostix.com>
2023-07-04 10:12:42 +0200

    iptables.sh example script

--- iptables.sh
+++ iptables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+SRC=10.0.0.1
+DST=10.0.0.2
+
 iptables-legacy -Z
 iptables-legacy -F
 iptables-legacy -N test
-iptables-legacy -A INPUT -s 10.0.0.1 -d 10.0.0.2 -j ACCEPT
+iptables-legacy -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# [...]
+# very large ruleset
+# [...]
+iptables-legacy -A INPUT -m state --state NEW -p tcp --dport 22 -s ${SRC} -d ${DST} -j ACCEPT
 
